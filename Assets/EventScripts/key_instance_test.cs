596b098d1b5e63e758b8b2459cb9736d048a5167
KAMONEGINABE <145260899+KAMONEGINABE@users.noreply.github.com>
2023-12-11 10:28:18 +0900

    新型のキー認識システム実装

--- Assets/EventScripts/key_instance_test.cs
+++ Assets/EventScripts/key_instance_test.cs
@@ -70,270 +70,87 @@ public class key_instance_test : MonoBehaviour
     {
         public string keyName;
         public KeyCode anyKeyCode;
-        public Vector3 keyPositon;
+        public Vector3 keyPosition;
 
         public KeyData(string newKeyName,KeyCode newAnyKeyCode,Vector3 newKeyPosition)
         {
             keyName = newKeyName;
             anyKeyCode = newAnyKeyCode;
-            KeyPosition = newKeyPosition;
+            keyPosition = newKeyPosition;
         }
     }
 
-    List<KeyData> KeyDataList = new List<KeyData>();
-    KeyList.Add( new KeyData( "Z",KeyCode.Z,new Vector3(-6.7f,0.5f,-1.9f)));
-    KeyList.Add( new KeyData( "X",KeyCode.X,new Vector3(-5.4f,0.5f,-1.9f)));
-    KeyList.Add( new KeyData( "C",KeyCode.C,new Vector3(-4.1f,0.5f,-1.9f)));
-    KeyList.Add( new KeyData( "V",KeyCode.V,new Vector3(-2.8f,0.5f,-1.9f)));
+        KeyData key_justNowPressed = new KeyData( "Neutral",KeyCode.None,new Vector3(9999f,9999f,9999f) );
+        KeyData key_oneTimeAgoPressed = new KeyData( "Neutral",KeyCode.None,new Vector3(9999f,9999f,9999f) );
+        KeyData key_twoTimeAgoPressed = new KeyData( "Neutral",KeyCode.None,new Vector3(9999f,9999f,9999f) );
+        KeyData key_threeTimeAgoPressed = new KeyData( "Neutral",KeyCode.None,new Vector3(9999f,9999f,9999f) );
+        ///<summary>
+        ///key_justNowPressedはUpdate_KeyMemory内でのみ使う。キーが入力されたらまず最初にUpdate_KeyMemoryを実行して、key_oneTimeAgoPressedにデータを移してから、そちらを参照すること。
+        ///</summary>
 
-    
-    KeyData key_justNowPressed = new KeyMemory( "Neutral",KeyCode.None,new Vector3(9999f,9999f,9999f) );//key_justNowPressedはUpdate_KeyMemory内でのみ使う。キーが入力されたらまず最初にUpdate_KeyMemoryを実行して、key_oneTimeAgoPressedにデータを移してから、そちらを参照すること。
-    KeyData key_oneTimeAgoPressed = new KeyMemory( "Neutral",KeyCode.None,new Vector3(9999f,9999f,9999f) );
-    KeyData key_twoTimeAgoPressed = new KeyMemory( "Neutral",KeyCode.None,new Vector3(9999f,9999f,9999f) );
-    KeyData key_threeTimeAgoPressed = new KeyMemory( "Neutral",KeyCode.None,new Vector3(9999f,9999f,9999f) );
     void Update_KeyMemory()
     {
-        Key_threeTimeAgoPressed = Key_twoTimeAgoPressed;
-        Key_twoTimeAgoPressed = Key_oneTimeAgoPressed;
-        Key_oneTimeAgoPressed = Key_justNowPressed;
-        print(Key_oneTimeAgoPressed);
+        key_threeTimeAgoPressed = key_twoTimeAgoPressed;
+        key_twoTimeAgoPressed = key_oneTimeAgoPressed;
+        key_oneTimeAgoPressed = key_justNowPressed;
+        print(key_oneTimeAgoPressed);
     }
+
+    List<KeyData> KeyDataList = new List<KeyData>();
 
     void Start()
     {
+        
+        KeyDataList.Add( new KeyData( "Z",KeyCode.Z,new Vector3(-6.7f,0.5f,-1.9f)));
+        KeyDataList.Add( new KeyData( "X",KeyCode.X,new Vector3(-5.4f,0.5f,-1.9f)));
+        KeyDataList.Add( new KeyData( "C",KeyCode.C,new Vector3(-4.1f,0.5f,-1.9f)));
+        KeyDataList.Add( new KeyData( "V",KeyCode.V,new Vector3(-2.8f,0.5f,-1.9f)));
+        KeyDataList.Add (new KeyData( "B", KeyCode.B, new Vector3(-1.5f, 0.5f, -1.9f)));
+        KeyDataList.Add( new KeyData( "N", KeyCode.N, new Vector3(-0.2f, 0.5f, -1.9f)));
+        KeyDataList.Add( new KeyData( "M", KeyCode.M, new Vector3(1.1f, 0.5f, -1.9f)));
+        KeyDataList.Add( new KeyData( "A", KeyCode.A, new Vector3(-7.3f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "S", KeyCode.S, new Vector3(-6.04f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "D", KeyCode.D, new Vector3(-4.78f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "F", KeyCode.F, new Vector3(-3.52f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "G", KeyCode.G, new Vector3(-2.26f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "H", KeyCode.H, new Vector3(-1f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "J", KeyCode.J, new Vector3(0.26f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "K", KeyCode.K, new Vector3(1.52f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "L", KeyCode.L, new Vector3(2.78f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "Q", KeyCode.Q, new Vector3(-7.95f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "W", KeyCode.W, new Vector3(-6.68f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "E", KeyCode.E, new Vector3(-5.41f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "R", KeyCode.R, new Vector3(-4.14f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "T", KeyCode.T, new Vector3(-2.87f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "Y", KeyCode.Y, new Vector3(-1.6f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "U", KeyCode.U, new Vector3(-0.33f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "I", KeyCode.I, new Vector3(0.94f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "O", KeyCode.O, new Vector3(2.21f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "P", KeyCode.P, new Vector3(3.48f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( "1", KeyCode.Alpha1, new Vector3(-8.5f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "2", KeyCode.Alpha2, new Vector3(-7.25f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "3", KeyCode.Alpha3, new Vector3(-6f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "4", KeyCode.Alpha4, new Vector3(-4.75f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "5", KeyCode.Alpha5, new Vector3(-3.5f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "6", KeyCode.Alpha6, new Vector3(-2.25f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "7", KeyCode.Alpha7, new Vector3(-1f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "8", KeyCode.Alpha8, new Vector3(0.25f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "9", KeyCode.Alpha9, new Vector3(1.5f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "0", KeyCode.Alpha0, new Vector3(2.75f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "-", KeyCode.Minus, new Vector3(4f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( "^", KeyCode.Caret, new Vector3(5.25f, 0.5f, 1.25f)));
+        KeyDataList.Add( new KeyData( ";", KeyCode.Semicolon, new Vector3(4.04f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( ":", KeyCode.Colon, new Vector3(5.3f, 0.5f, -0.87f)));
+        KeyDataList.Add( new KeyData( "@", KeyCode.At, new Vector3(4.75f, 0.5f, 0.16f)));
+        KeyDataList.Add( new KeyData( ",", KeyCode.Comma, new Vector3(2.4f, 0.5f, -1.9f)));
+        KeyDataList.Add( new KeyData( ".", KeyCode.Period, new Vector3(3.7f, 0.5f, -1.9f)));
+        KeyDataList.Add( new KeyData( "/", KeyCode.Slash, new Vector3(5f, 0.5f, -1.9f)));
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (MyInput.MyInputKeyDown(KeyCode.Z)) = 
-        {
-            KeyData foundKeyData = keyList.Find(key => key.anyKeyCode == KeyCode.C);
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.X))
-        {
-            key_justNowPressed.overwriteKeyMemory( "X",new Vector3(-5.4f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.C))
-        {
-            key_justNowPressed.overwriteKeyMemory( "C",new Vector3(-4.1f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.V))
-        {
-            key_justNowPressed.overwriteKeyMemory( "V",new Vector3(-2.8f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.B))
-        {
-            key_justNowPressed.overwriteKeyMemory( "B",new Vector3(-1.5f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.N))
-        {
-            key_justNowPressed.overwriteKeyMemory( "N",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.M))
-        {
-            key_justNowPressed.overwriteKeyMemory( "M",new Vector3(1.1f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.A))
-        {
-            key_justNowPressed.overwriteKeyMemory( "A",new Vector3(-7.3f,0.5f,-0.87f) );
-
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.S))
-        {
-            key_justNowPressed.overwriteKeyMemory( "S",new Vector3(-6.04f,0.5f,-0.87f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.D))
-        {
-            key_justNowPressed.overwriteKeyMemory( "D",new Vector3(-4.78f,0.5f,-0.87f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.F))
-        {
-            key_justNowPressed.overwriteKeyMemory( "F",new Vector3(-3.52f,0.5f,-0.87f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.G))
-        {
-            key_justNowPressed.overwriteKeyMemory( "G",new Vector3(-2.26f,0.5f,-0.87f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.H))
-        {
-            key_justNowPressed.overwriteKeyMemory( "H",new Vector3(-1f,0.5f,-0.87f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.J))
-        {
-            key_justNowPressed.overwriteKeyMemory( "J",new Vector3(0.26f,0.5f,-0.87f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.K))
-        {
-            key_justNowPressed.overwriteKeyMemory( "K",new Vector3(1.52f,0.5f,-0.87f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.L))
-        {
-            key_justNowPressed.overwriteKeyMemory( "L",new Vector3(2.78f,0.5f,-0.87f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Q))
-        {
-            key_justNowPressed.overwriteKeyMemory( "Q",new Vector3(-7.95f,0.5f,0.16f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.W))
-        {
-            key_justNowPressed.overwriteKeyMemory( "W",new Vector3(-6.68f,0.5f,0.16f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.E))
-        {
-            key_justNowPressed.overwriteKeyMemory( "E",new Vector3(-5.41f,0.5f,0.16f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.R))
-        {
-            key_justNowPressed.overwriteKeyMemory( "R",new Vector3(-4.14f,0.5f,0.16f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.T))
-        {
-            key_justNowPressed.overwriteKeyMemory( "T",new Vector3(-2.87f,0.5f,0.16f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Y))
-        {
-            key_justNowPressed.overwriteKeyMemory( "Y",new Vector3(-1.6f,0.5f,0.16f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.U))
-        {
-            key_justNowPressed.overwriteKeyMemory( "U",new Vector3(-0.33f,0.5f,0.16f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.I))
-        {
-            print("I");
-            Update_KeyMemory(new Vector3(0.94f,0.5f,0.16f));
-            key_justNowPressed.overwriteKeyMemory( "I",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.O))
-        {
-            print("O");
-            Update_KeyMemory(new Vector3(2.21f,0.5f,0.16f));
-            key_justNowPressed.overwriteKeyMemory( "O",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.P))
-        {
-            print("P");
-            Update_KeyMemory(new Vector3(3.48f,0.5f,0.16f));
-            key_justNowPressed.overwriteKeyMemory( "P",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha1))
-        {
-            print("1");
-            Update_KeyMemory(new Vector3(-8.5f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "1",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha2))
-        {
-            print("2");
-            Update_KeyMemory(new Vector3(-7.25f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "2",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha3))
-        {
-            print("3");
-            Update_KeyMemory(new Vector3(-6f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "3",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha4))
-        {
-            print("4");
-            Update_KeyMemory(new Vector3(-4.75f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "4",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha5))
-        {
-            print("5");
-            Update_KeyMemory(new Vector3(-3.5f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "5",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha6))
-        {
-            print("6");
-            Update_KeyMemory(new Vector3(-2.25f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "6",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha7))
-        {
-            print("7");
-            Update_KeyMemory(new Vector3(-1f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "7",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha8))
-        {
-            print("8");
-            Update_KeyMemory(new Vector3(0.25f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "8",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha9))
-        {
-            print("9");
-            Update_KeyMemory(new Vector3(1.5f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "9",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Alpha0))
-        {
-            print("0");
-            Update_KeyMemory(new Vector3(2.75f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "0",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Minus))
-        {
-            print("-");
-            Update_KeyMemory(new Vector3(4f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "-",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Caret))
-        {
-            print("^");
-            Update_KeyMemory(new Vector3(5.25f,0.5f,1.25f));
-            key_justNowPressed.overwriteKeyMemory( "^",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-
-         if (MyInput.MyInputKeyDown(KeyCode.Semicolon))
-        {
-            print(";");
-            Update_KeyMemory(new Vector3(4.04f,0.5f,-0.87f));
-            key_justNowPressed.overwriteKeyMemory( ";",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Colon))
-        {
-            print(":");
-            Update_KeyMemory(new Vector3(5.3f,0.5f,-0.87f));
-            key_justNowPressed.overwriteKeyMemory( ":",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-
-         if (MyInput.MyInputKeyDown(KeyCode.At))
-        {
-            print("@");
-            Update_KeyMemory(new Vector3(4.75f,0.5f,0.16f));
-            key_justNowPressed.overwriteKeyMemory( "N",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-
-         if (MyInput.MyInputKeyDown(KeyCode.Comma))
-        {
-            print(",");
-            Update_KeyMemory(new Vector3(2.4f,0.5f,-1.9f));
-            key_justNowPressed.overwriteKeyMemory( "N",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-         if (MyInput.MyInputKeyDown(KeyCode.Period))
-        {
-            print(".");
-            Update_KeyMemory(new Vector3(3.7f,0.5f,-1.9f));
-            key_justNowPressed.overwriteKeyMemory( "N",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-
-         if (MyInput.MyInputKeyDown(KeyCode.Slash))
-        {
-            print("/");
-            Update_KeyMemory(new Vector3(5f,0.5f,-1.9f));
-            key_justNowPressed.overwriteKeyMemory( "N",new Vector3(-0.2f,0.5f,-1.9f) );
-        }
-        //trueじゃなければHPマイナス
-        if (MyInput.MyInputKeydown(KeyCode.Space, 1 / 2))
-        {
-            print("3秒待つと押せるよ");
-        }
+        
     }
 }
