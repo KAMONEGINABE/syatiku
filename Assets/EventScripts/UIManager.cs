c305c18b7d13536b532a84c774e5aad27e19e80b
KAMONEGINABE <145260899+KAMONEGINABE@users.noreply.github.com>
2023-12-25 07:21:19 +0900

    キャンバス追加したりした（Sceneファイルがアップロードできてないかも。一応ギガファイル便化しておこうかな）

--- Assets/EventScripts/UIManager.cs
+++ Assets/EventScripts/UIManager.cs
@@ -8,9 +8,24 @@ public class UIManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI scoreUI;
     public void drawResultCanvas()
     {
+        var statusManager = GameObject.FindObjectOfType<statusManager>();
         statusManager.resultStatus.endgameResultStatus result = statusManager.resultStatusInstance.createEndgameResultStatus();
+
+        print("スコア："+result.endgameScore);
+        print("社員数："+result.endgameEmployeeNumber);
+        print("脱走阻止数："+result.endgamePreventEscapeNumber);
+        print("脱走成功数："+result.endgameSucceedEscapeNumber);
+        print("総ダメージ量："+result.endgameTotalDamage);
         
-        
+        GameObject inGameCanvas = GameObject.Find("inGameCanvas");
+        var componentInGameCanvas = inGameCanvas.GetComponent<Canvas>();
+        componentInGameCanvas.enabled = false;
+
+        print(inGameCanvas);
+
+        GameObject resultCanvas = GameObject.Find("resultCanvas");
+        var componentResultCanvas = resultCanvas.GetComponent<Canvas>();
+        componentResultCanvas.enabled = true;
     }
     
     void Start()
--- Assets/EventScripts/gameRuleManager.cs
+++ Assets/EventScripts/gameRuleManager.cs
@@ -46,14 +46,9 @@ public class gameRuleManager : MonoBehaviour
             var keyInputManager = GameObject.FindObjectOfType<keyInputManager>();
             keyInputManager.enabled = false;
             
-            var statusManager = GameObject.FindObjectOfType<statusManager>();
-            statusManager.resultStatus.endgameResultStatus result = statusManager.resultStatusInstance.createEndgameResultStatus();
+            var UIManager = GameObject.FindObjectOfType<UIManager>();
+            UIManager.drawResultCanvas();
             print("ゲーム終了！");
-            print("スコア："+result.endgameScore);
-            print("社員数："+result.endgameEmployeeNumber);
-            print("脱走阻止数："+result.endgamePreventEscapeNumber);
-            print("脱走成功数："+result.endgameSucceedEscapeNumber);
-            print("総ダメージ量："+result.endgameTotalDamage);
 
             var gameRuleManager = GameObject.FindObjectOfType<gameRuleManager>();
             gameRuleManager.enabled = false;
